a2b601cdd74442b200bf5510e9f84e865874708e
Jakub Niewczas <niewczas.jakub@gmail.com>
2020-09-17 11:00:57 +0900

    use Header on Homescreen

--- src/frontend/components/screens/Home/index.jsx
+++ src/frontend/components/screens/Home/index.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import Header from "@/components/Header";
 import GameSection from "./GameSection";
 
 import "./styles.scss";
 
 const HomeScreen = () => (
   <div styleName="wrapper">
+    <Header fixed />
     <GameSection />
   </div>
 );
