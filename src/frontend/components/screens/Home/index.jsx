1beede58bac04038871673905ecc6944d02a3491
Jakub Niewczas <niewczas.jakub@gmail.com>
2020-09-17 10:41:16 +0900

    extract part of homescreen to GameSection component

--- src/frontend/components/screens/Home/index.jsx
+++ src/frontend/components/screens/Home/index.jsx
@@ -1,21 +1,11 @@
 import React from "react";
-import Button from "@/components/Button";
+import GameSection from "./GameSection";
 
 import "./styles.scss";
 
 const HomeScreen = () => (
   <div styleName="wrapper">
-    <h1 styleName="header">
-      &quot;I can help&nbsp;
-      <b>Indeed!</b>
-      &quot;
-    </h1>
-    <p>additional description</p>
-
-    <div styleName="mainMenu">
-      <Button styleName="button">Story Mode</Button>
-      <Button styleName="button">Quick Game</Button>
-    </div>
+    <GameSection />
   </div>
 );
 
