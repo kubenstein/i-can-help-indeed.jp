27cbc65043733cd5106df358636826c02cb68a9d
Jakub Niewczas <niewczas.jakub@gmail.com>
2020-09-17 11:00:06 +0900

    configure app routing

--- src/frontend/components/App/index.jsx
+++ src/frontend/components/App/index.jsx
@@ -1,8 +1,17 @@
 import React from "react";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
 import HomeScreen from "@/components/screens/Home";
 
 import "./global.scss";
 
-const App = () => <HomeScreen />;
+const App = () => (
+  <Router>
+    <Switch>
+      <Route path="/">
+        <HomeScreen />
+      </Route>
+    </Switch>
+  </Router>
+);
 
 export default App;
