5ded5f12e47dc09d1fdf539e98f72aa5b50a0ed1
Jakub Niewczas <niewczas.jakub@gmail.com>
2020-09-21 00:16:55 +0900

    improve readability of WATER_FLOWN event handler
    - reorder finctions
    - name conditions

--- src/frontend/components/Game/evenHandlers/waterFlowed.js
+++ src/frontend/components/Game/evenHandlers/waterFlowed.js
@@ -1,35 +1,7 @@
+/* eslint-disable no-use-before-define */
 import deepCopy from "@/lib/deepCopy";
 import { tileTypes } from "../tile";
 import statuses from "../statuses";
-
-const findStart = (board) => {
-  for (let y = 0; y < board.length; y += 1) {
-    for (let x = 0; x < board[y].length; x += 1) {
-      if (board[y][x].type === tileTypes.start) return [x, y];
-    }
-  }
-  return { x: undefined, y: undefined };
-};
-
-const flowWater = (x, y, from, currentBoard) => {
-  const board = deepCopy(currentBoard);
-
-  if (!board?.[y]?.[x] || !board[y][x].flowOutputFor[from]) return { board, status: statuses.failed };
-
-  if (board[y][x].fill) {
-    const direction = board[y][x].flowOutputFor[from];
-    if (direction === "N") return flowWater(x, y - 1, "S", board);
-    if (direction === "E") return flowWater(x + 1, y, "W", board);
-    if (direction === "S") return flowWater(x, y + 1, "N", board);
-    if (direction === "W") return flowWater(x - 1, y, "E", board);
-  } else {
-    board[y][x].fill = true;
-  }
-
-  if (board[y][x].type === tileTypes.finish) return { board, status: statuses.won };
-
-  return { board, status: statuses.running };
-};
 
 export default (gameEngine) => {
   const state = gameEngine.getState();
@@ -43,3 +15,38 @@ export default (gameEngine) => {
 
   return state;
 };
+
+const flowWater = (x, y, from, currentBoard) => {
+  const board = deepCopy(currentBoard);
+
+  if (outOfBoard(x, y, board) || notConnected(x, y, from, board)) return { board, status: statuses.failed };
+
+  if (board[y][x].fill) {
+    const direction = board[y][x].flowOutputFor[from];
+    if (direction === "N") return flowWater(x, y - 1, "S", board);
+    if (direction === "E") return flowWater(x + 1, y, "W", board);
+    if (direction === "S") return flowWater(x, y + 1, "N", board);
+    if (direction === "W") return flowWater(x - 1, y, "E", board);
+  } else {
+    board[y][x].fill = true;
+  }
+
+  if (reachedFinish(x, y, board)) return { board, status: statuses.won };
+
+  return { board, status: statuses.running };
+};
+
+// utils
+
+const findStart = (board) => {
+  for (let y = 0; y < board.length; y += 1) {
+    for (let x = 0; x < board[y].length; x += 1) {
+      if (board[y][x].type === tileTypes.start) return [x, y];
+    }
+  }
+  return { x: undefined, y: undefined };
+};
+
+const outOfBoard = (x, y, board) => !board?.[y]?.[x];
+const notConnected = (x, y, from, board) => !board[y][x].flowOutputFor[from];
+const reachedFinish = (x, y, board) => board[y][x].type === tileTypes.finish;
