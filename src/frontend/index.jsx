f902049938ac80595fd5e5e0c41c443970da251e
Jakub Niewczas <niewczas.jakub@gmail.com>
2020-09-21 18:12:06 +0900

    add favicon

--- src/frontend/index.jsx
+++ src/frontend/index.jsx
@@ -4,6 +4,7 @@ import ReactDOM from "react-dom";
 import App from "@/components/App";
 
 import "./assets/jakub-niewczas-sr-frontend-engineer.pdf";
+import "./assets/favicon.ico";
 import "./index.html";
 
 ReactDOM.render(<App />, document.getElementById("app"));
