5d8beb6edc1f1860c9749c627b1b6a83bd47717e
Jakub Niewczas <niewczas.jakub@gmail.com>
2022-03-24 18:57:34 +0900

    remove www redirection

--- src/backend/utils/redirectToHttpsAndWww.js
+++
@@ -1,7 +0,0 @@
-export default (req, res, next) => {
-  if (process.env.NODE_ENV !== "production") return next();
-
-  if (!req.headers.host.match(/^www\..*/i) || req.headers["x-forwarded-proto"] === "http")
-    return res.redirect(301, `${process.env.PAGE_URL}${req.url}`);
-  return next();
-};
