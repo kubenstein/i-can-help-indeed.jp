cc364bd8a0956ca6b2df660e3ba56406b575de05
Jakub Niewczas <niewczas.jakub@gmail.com>
2024-12-09 16:50:17 +0900

    remove password

--- src/frontend/components/App/index.jsx
+++ src/frontend/components/App/index.jsx
@@ -1,48 +1,29 @@
-import React, { useEffect, useMemo, useState } from "react";
+import React from "react";
 import { HashRouter as Router, Switch, Route } from "react-router-dom";
-import qs from "qs";
 import HomeScreen from "@/components/screens/Home";
-import AuthRequiredScreen from "@/components/screens/AuthRequiredScreen";
 import StoryScreen from "@/components/screens/Story";
 import GameScreen from "@/components/screens/Game";
 import TechnicalDocumentationScreen from "@/components/screens/TechnicalDocumentation";
 
 import "./global.scss";
 
-const basicAuthSecretReverted = "sazcweinbukaj";
-
-const App = () => {
-  const [password, setPassword] = useState(() => {
-    const passwordUrlParam = qs.parse(window.location.hash.split("?")[1]).password;
-    const passwordLocalStorage = window.localStorage.getItem("indeed-password");
-    return passwordUrlParam || passwordLocalStorage;
-  });
-  const isBasicAuthed = useMemo(() => password === basicAuthSecretReverted.split("").reverse().join(""), [password]);
-
-  useEffect(() => {
-    if (password) window.localStorage.setItem("indeed-password", password);
-  }, [password]);
-
-  return isBasicAuthed ? (
-    <Router>
-      <Switch>
-        <Route path="/story">
-          <StoryScreen />
-        </Route>
-        <Route path="/game">
-          <GameScreen />
-        </Route>
-        <Route path="/technical-documentation">
-          <TechnicalDocumentationScreen />
-        </Route>
-        <Route path="/">
-          <HomeScreen />
-        </Route>
-      </Switch>
-    </Router>
-  ) : (
-    <AuthRequiredScreen onPasswordChange={setPassword} />
-  );
-};
+const App = () => (
+  <Router>
+    <Switch>
+      <Route path="/story">
+        <StoryScreen />
+      </Route>
+      <Route path="/game">
+        <GameScreen />
+      </Route>
+      <Route path="/technical-documentation">
+        <TechnicalDocumentationScreen />
+      </Route>
+      <Route path="/">
+        <HomeScreen />
+      </Route>
+    </Switch>
+  </Router>
+);
 
 export default App;
